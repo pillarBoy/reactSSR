ac8467aa09b13f5f9cc31f4d3cedc18a1a61a842
pillarBoy <601991726@qq.com>
2019-12-29 21:08:53 +0800

    app init

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import { Button } from 'antd'
+
+export default () => (
+  <div style={{ marginTop: 100 }}>
+    <Button>ok</Button>
+  </div>
+)
--- server/csrRender.js
+++
@@ -1,9 +0,0 @@
-
-import path from 'path'
-import fs from 'fs'
-
-export default function csrRender(res) {
-  console.log('csr')
-  const filename = path.resolve(process.cwd(), 'public/index.csr.html')
-  res.send(fs.readFileSync(filename, 'utf8'))
-}
